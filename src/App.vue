083196e5ef18a5a50abe0f4a27cf01ee944da797
Nick Hand <nicholas.adam.hand@gmail.com>
2021-09-13 16:54:42 -0400

    add sections

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,46 @@
 <template>
   <v-app>
-    <v-main> </v-main>
+    <v-main>
+      <!-- 1. Introduction -->
+      <Intro />
+
+      <!-- 2. Key Takeaways -->
+      <KeyTakeaways />
+
+      <!-- 3. Cash Balances -->
+      <CashBalances />
+
+      <!-- 4. General Fund -->
+      <GeneralFund />
+
+      <!-- 5. Conclusion -->
+      <Conclusion />
+
+      <!-- 6. Footnotes -->
+      <Footnotes />
+    </v-main>
   </v-app>
 </template>
 
 <script>
+import Intro from "@/sections/1-Intro";
+import KeyTakeaways from "@/sections/2-KeyTakeaways";
+import CashBalances from "@/sections/3-CashBalances";
+import GeneralFund from "@/sections/4-GeneralFund";
+import Conclusion from "@/sections/5-Conclusion";
+import Footnotes from "@/sections/6-Footnotes";
+
 export default {
   name: "CashReportApp",
 
-  components: {},
+  components: {
+    Intro,
+    KeyTakeaways,
+    CashBalances,
+    GeneralFund,
+    Conclusion,
+    Footnotes,
+  },
 };
 </script>
 
@@ -25,5 +57,8 @@ export default {
 }
 .card-footer {
   font-size: 0.9rem;
+}
+.section {
+  margin-top: 2.5rem;
 }
 </style>
---
+++ src/sections/5-Conclusion.vue
@@ -0,0 +1,51 @@
+<template>
+  <!-- Summary -->
+  <div class="section">
+    <h2>Conclusion</h2>
+    <hr class="titlebar" />
+
+    <div class="section-text">
+      <p>
+        At the close of the first full fiscal year following the COVID-19
+        pandemic, the City’s cash position remained strong. While the pandemic
+        has certainly impacted City finances, resulting in revenue declines and
+        spending cuts, the impact was not as large as originally anticipated in
+        June 2020. The City managed to end FY21 with its highest cash balance
+        for the General Fund since at least FY07, $15M higher than the
+        pre-pandemic level from FY19. With the passage of the American Rescue
+        Plan, the City also received the first half of its $1.4B in federal
+        relief, bringing the Grants Fund balance to an unprecedented $950M at
+        the end of FY21. Altogether, this resulted in the City closing FY21 with
+        a Consolidated Cash balance of more than $2.2B, its highest since at
+        least FY07.
+      </p>
+      <p>
+        While economic activity has increased from its low point in summer 2020,
+        the City and Commonwealth have recently re-implemented a number of
+        public health restrictions. As the city continues its recovery from the
+        pandemic, unemployment remains high and collections for the majority of
+        taxes are still below pre-pandemic baselines. In the coming fiscal year,
+        the City can use funds from the American Rescue Plan to fill any
+        additional revenue shortfalls caused by the pandemic and restore
+        services and cuts to departmental spending. Previously, the Controller’s
+        Office released a
+        <a
+          href="https://controller.phila.gov/philadelphia-audits/american-rescue-plan-recommendations/"
+          target="_blank"
+          >report</a
+        >
+        with recommendations on how to best spend this unprecedented influx of
+        federal funding. In future cash reports, the office will continue to
+        monitor the City’s cash position, including the impact of the federal
+        relief on the City’s finances.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
