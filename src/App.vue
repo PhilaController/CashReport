3a553ea1182054cbb74a508348b913605596b0d9
Nick Hand <nicholas.adam.hand@gmail.com>
2021-09-13 15:38:18 -0400

    add README

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main> </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "CashReportApp",
+
+  components: {},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.header-title {
+  font-size: 1.7rem;
+  font-family: inherit;
+  font-weight: 500;
+  margin-right: 1rem;
+}
+.header-subtitle {
+  font-style: italic;
+  font-size: 1rem;
+}
+.card-footer {
+  font-size: 0.9rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
-import App from './App.vue'
+import '@/plugins/composition'
+import '@/plugins/chartjs'
+import vuetify from '@/plugins/vuetify'
+import App from '@/App.vue'
+import $ from "jquery"
 
 Vue.config.productionTip = false
 
+// load and set the HTML template we are using
+let audit_content = $(".audit-content");
+audit_content.html(`<div id="app"></div>`);
+
+function add_help_message() {
+
+  if ($(".help-message").length > 0) return;
+
+  let helpMessage = `<p class='help-message'>
+  Comments or feedback? Please contact
+  <a href="mailto:controller@phila.gov">controller@phila.gov</a>.
+  </p>`;
+  $(".back-link").after(helpMessage);
+}
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
+
+// add a help message
+add_help_message();
