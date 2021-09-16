5dc9ff2d33c56bbd77df7ad4b1ed97cb0f6bb5ea
Nick Hand <nicholas.adam.hand@gmail.com>
2021-09-16 13:59:41 -0400

    add past report links

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,51 @@
 <template>
   <v-app>
     <v-main>
+      <!-- FY21 -->
       <FY21Q4 v-if="reportTag == 'FY21-Q4'" />
       <FY21Q3 v-if="reportTag == 'FY21-Q3'" />
+      <FY21Q2 v-if="reportTag == 'FY21-Q2'" />
+      <FY21Q1 v-if="reportTag == 'FY21-Q1'" />
+
+      <!-- FY20 -->
+      <FY20Q4 v-if="reportTag == 'FY20-Q4'" />
+      <FY20Q2 v-if="reportTag == 'FY20-Q2'" />
+      <FY20Q1 v-if="reportTag == 'FY20-Q1'" />
+
+      <!-- FY19 -->
+      <FY19Q4 v-if="reportTag == 'FY19-Q4'" />
+      <FY19Q3 v-if="reportTag == 'FY19-Q3'" />
+      <FY19Q2 v-if="reportTag == 'FY19-Q2'" />
+      <FY19Q1 v-if="reportTag == 'FY19-Q1'" />
+
+      <!-- FY18 -->
+      <FY18Q4 v-if="reportTag == 'FY18-Q4'" />
     </v-main>
   </v-app>
 </template>
 
 <script>
+// FY21
 import FY21Q4 from "@/reports/FY21-Q4";
 import FY21Q3 from "@/reports/FY21-Q3";
+import FY21Q2 from "@/reports/FY21-Q2";
+import FY21Q1 from "@/reports/FY21-Q1";
+
+// FY20
+import FY20Q4 from "@/reports/FY20-Q4";
+import FY20Q2 from "@/reports/FY20-Q2";
+import FY20Q1 from "@/reports/FY20-Q1";
+
+// FY19
+import FY19Q4 from "@/reports/FY19-Q4";
+import FY19Q3 from "@/reports/FY19-Q3";
+import FY19Q2 from "@/reports/FY19-Q2";
+import FY19Q1 from "@/reports/FY19-Q1";
+
+// FY18
+import FY18Q4 from "@/reports/FY18-Q4";
+
+// Utils
 import { getReportTag } from "@/utils/";
 
 export default {
@@ -17,7 +53,20 @@ export default {
   data() {
     return { reportTag: getReportTag() };
   },
-  components: { FY21Q4, FY21Q3 },
+  components: {
+    FY21Q4,
+    FY21Q3,
+    FY21Q2,
+    FY21Q1,
+    FY20Q4,
+    FY20Q2,
+    FY20Q1,
+    FY19Q4,
+    FY19Q3,
+    FY19Q2,
+    FY19Q1,
+    FY18Q4,
+  },
 };
 </script>
 
@@ -35,7 +84,7 @@ export default {
 .card-footer {
   font-size: 0.9rem;
 }
-.section {
+.section:nth-child(n + 2) {
   margin-top: 2.5rem;
 }
 </style>
