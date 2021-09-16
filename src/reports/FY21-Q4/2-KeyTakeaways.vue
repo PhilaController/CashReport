7d2633cd33c0e6efa5919b47c7f63280a8f7d629
Nick Hand <nicholas.adam.hand@gmail.com>
2021-09-16 11:17:43 -0400

    add FY21 Q4 files

---
+++ src/reports/FY21-Q4/2-KeyTakeaways.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="section">
+    <h2>Key Takeaways</h2>
+    <hr class="titlebar" />
+
+    <ul>
+      <li>
+        The City’s cash position remained strong at the end of FY21, with the
+        General Fund ending the year with its highest cash balance since at
+        least FY07. Due primarily to higher-than-expected revenue and
+        lower-than-expected spending, the actual end-of-year cash balance for
+        the General Fund was $476M higher than the projection from the adopted
+        FY21 budget in June 2020.
+      </li>
+      <li>
+        The Grants Fund ended FY21 with a cash balance of $950M, its highest
+        since at least FY07. The balance was boosted by the City receiving the
+        first half of its $1.4B in federal relief as part of the American Rescue
+        Plan.
+      </li>
+      <li>
+        The Consolidated Cash account, the City’s combined cash fund, ended FY21
+        with a balance of more than $2.2B, its highest since at least FY07, as a
+        result of the historically high cash balances for the General Fund and
+        Grants Fund.
+      </li>
+      <li>
+        Due to budget cuts enacted in response to the COVID-19 pandemic, total
+        cash spending declined by 3% in FY21. Total cash revenue for the General
+        Fund increased by 6.2% relative to FY20. Primarily, this increase is a
+        result of the City delaying the filing deadline for business taxes in
+        FY20 to FY21, which lead to additional payments of these taxes during
+        FY21.
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default { name: "KeyTakeaways" };
+</script>
+
---
+++ src/reports/FY21-Q4/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Intro />
+    <KeyTakeaways />
+    <CashBalances />
+    <GeneralFund />
+    <Conclusion />
+    <Footnotes />
+  </div>
+</template>
+
+<script>
+import Intro from "./1-Intro";
+import KeyTakeaways from "./2-KeyTakeaways";
+import CashBalances from "./3-CashBalances";
+import GeneralFund from "./4-GeneralFund";
+import Conclusion from "./5-Conclusion";
+import Footnotes from "./6-Footnotes";
+
+export default {
+  components: {
+    Intro,
+    KeyTakeaways,
+    CashBalances,
+    GeneralFund,
+    Conclusion,
+    Footnotes,
+  },
+};
+</script>
+
